e658a537986eab90dde25e4fbd19bb884497f2b2
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2022-04-28 12:49:11 -0400

    updated script

--- src/memsearch.js
+++ src/memsearch.js
@@ -5,27 +5,34 @@ const m = Process.enumerateModules()[0];
 //const m = Module.load('win32u.dll');
 
 // Print its properties:
-console.log(JSON.stringify(m));
+const properties = JSON.stringify(m);
+console.log(properties);
+
+send({type:"properties", data:properties});
 
 // Dump it from its base address:
-console.log(hexdump(m.base));
+const dump = hexdump(m.base);
+console.log(dump);
 
-// The pattern that you are interested in:
-const pattern = '00 00 00 00 ?? 13 37 ?? 42';
+send({type:"dump", data:dump});
 
-Memory.scan(m.base, m.size, pattern, {
-  onMatch(address, size) {
-    console.log('Memory.scan() found match at', address,
-        'with size', size);
 
-    // Optionally stop scanning early:
-    return 'stop';
-  },
-  onComplete() {
-    console.log('Memory.scan() complete');
-  }
-});
-
-const results = Memory.scanSync(m.base, m.size, pattern);
-console.log('Memory.scanSync() result:\n' +
-    JSON.stringify(results));
+// // The pattern that you are interested in:
+// const pattern = '00 00 00 00 ?? 13 37 ?? 42';
+//
+// Memory.scan(m.base, m.size, pattern, {
+//   onMatch(address, size) {
+//     console.log('Memory.scan() found match at', address,
+//         'with size', size);
+//
+//     // Optionally stop scanning early:
+//     return 'stop';
+//   },
+//   onComplete() {
+//     console.log('Memory.scan() complete');
+//   }
+// });
+//
+// const results = Memory.scanSync(m.base, m.size, pattern);
+// console.log('Memory.scanSync() result:\n' +
+//     JSON.stringify(results));
